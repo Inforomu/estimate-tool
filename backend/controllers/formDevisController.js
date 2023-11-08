6b062c5c31687410e5aa9da8eca8fe777f3b6f60
rochaya <olivier.g77580@gmail.com>
2023-11-08 04:58:42 +0100

    formulaire ok , faire tailwind css, gerer la liaison avec author et client_id

--- backend/controllers/formDevisController.js
+++ backend/controllers/formDevisController.js
@@ -5,7 +5,23 @@ exports.submitForm = async (req, res) => {
         console.log('Données reçues du formulaire :', req.body);
         const formData = new FormData({
             power_contract: req.body.power_contract,
-            power_yg: req.body.power_yg
+            power_yg: req.body.power_yg,
+            contract: req.body.contract,
+            electric_controller: req.body.electric_controller,
+            telereport: req.body.telereport,
+            wifi: req.body.wifi,
+            mobile: req.body.mobile,
+            ground_res: req.body.ground_res,
+            neutral_system: req.body.neutral_system,
+            breaker: req.body.breaker,
+            distance: req.body.distance,
+            secure: req.body.secure,
+            type_e: req.body.type_e,
+            dispo_td: req.body.dispo_td,
+            power_charging: req.body.power_charging,
+            charge_points: req.body.charge_points,
+            box_nb: req.body.box_nb
+
         });
         const formDataResult = await formData.save();
         const formDataId = formDataResult.insertId;
