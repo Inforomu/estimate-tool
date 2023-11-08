ec9a4d969f30dd4cae9247d64d9a95def31c7709
MDFLAMorANGELS <valentinmd78@gmail.com>
2023-11-08 01:04:59 +0100

    Ajout d un menu en test

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,14 +1,51 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Logout from '../components/Logout';
 import { Link } from "react-router-dom";
 
 export default function Home() {
-  return (
-    <section className='flex flex-col'>
-        <h2 className='mb-3 text-2xl font-semibold'>Accueil</h2>
-        <Link to="/formdevis" className="text-white bg-green-700 my-2 hover:bg-green-800 focus:ring-4 focus:outline-none focus:ring-green-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center dark:bg-green-600 dark:hover:bg-green-700 dark:focus:ring-green-800" >Créer un Devis</Link>
-        <Logout/>
-    </section>
 
+  const [menuDevis, setMenuDevis] = useState(false);
+  const [menuClients, setMenuClients] = useState(false);
+
+  const handleMenuDevis = () => {
+    if (menuDevis === false) {
+      setMenuClients(false);
+      setMenuDevis(true);
+    } else {
+      setMenuDevis(false);
+    }
+  };
+  const handleMenuClients = () => {
+    if (menuClients === false) {
+      setMenuDevis(false);
+      setMenuClients(true);
+    } else {
+      setMenuClients(false);
+    }
+  };
+
+  return (
+    <section className=''>
+      <h2 className='mb-3 text-2xl font-semibold'>Accueil</h2>
+      <div className='flex'>
+        <div className='flex flex-col'>
+          <button onClick={handleMenuClients} className=' bg-green-500 m-1 px-5 py-3 rounded-lg shadow font-semibold text-center hover:bg-green-600'>Clients</button>
+          <button onClick={handleMenuDevis} className=' bg-green-500 m-1 px-5 py-3 rounded-lg shadow font-semibold text-center hover:bg-green-600'>Devis</button>
+          <Logout />
+        </div>
+        {menuDevis && (
+          <div className='flex flex-col'>
+            <Link to={"/formdevis"} className='bg-green-500 m-1 px-5 py-3 rounded-lg shadow font-semibold text-center hover:bg-green-600'>Créer un Devis</Link>
+            <Link to={"/devis"} className='bg-green-500 m-1 px-5 py-3 rounded-lg shadow font-semibold text-center hover:bg-green-600'>Voir les Devis</Link>
+          </div>
+        )}
+        {menuClients && (
+          <div className='flex flex-col'> 
+            <Link to={"/formclient"} className='bg-green-500 m-1 px-5 py-3 rounded-lg shadow font-semibold text-center hover:bg-green-600'>Créer un Client</Link>
+            <Link to={"/clients"} className='bg-green-500 m-1 px-5 py-3 rounded-lg shadow font-semibold text-center hover:bg-green-600'>Voir les Clients</Link>
+          </div>
+        )}
+      </div>
+    </section>
   )
 }
